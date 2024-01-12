9b1a3aff95777f6e870721f48a446496c9754820
hunterxcobby <solomonsefah13@gmail.com>
2024-01-12 22:23:59 +0000

    script to create an instance

---
+++ sql_alchemy/practice/create_instance.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from create_schema import User
+
+ed_user = User(name="Cobby", fullname="Cobby Sefah Solomon", nickname="hunterxcobby", age=21)
+
+print(ed_user.name)
+print(ed_user.fullname)
+print(ed_user.nickname)
+print(ed_user.age)
+print(str(ed_user.id))
+
+
+
+
